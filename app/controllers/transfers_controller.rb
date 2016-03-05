9ae174a7885130beac35ba5b11ea3d6e740ca327
dpserretti <daniel.dink91@gmail.com>
2016-03-05 05:45:44 -0300

    adidicona ação transferencia

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,4 +1,4 @@
-class TranfersController < ApplicationController
+class TransfersController < ApplicationController
   # before_action :set_account, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
@@ -6,6 +6,9 @@ class TranfersController < ApplicationController
   end
 
   def withdraw
+  end
+
+  def transfer
   end
 
   def depositar
@@ -24,16 +27,45 @@ class TranfersController < ApplicationController
   end
 
   def sacar
-    @deposit = Transfer.new(withdraw_params)
+    @withdraw = Transfer.new(withdraw_params)
     valor = deposit_params[:value]
     @account = Account.find(withdraw_params[:origin_account])
     valor = valor.to_f
     @account.balance -= valor
 
+    if @account.balance < 0
+      flash[:notice] = "Saldo insuficiente."
+      # redirect_to accounts_path
+    end
+
     if @account.save
-      if @deposit.save
+      if @withdraw.save
         flash[:notice] = "Saque realizado."
         redirect_to accounts_path
+      end
+    end
+  end
+
+  def transferir
+    @transfer = Transfer.new(transfer_params)
+    valor = deposit_params[:value]
+    valor = valor.to_f
+    @origin = Account.find(transfer_params[:origin_account])
+    @destiny = Account.find(transfer_params[:destiny_account])
+    @origin.balance -= valor
+    @destiny.balance += valor
+
+    if @origin.balance < 0
+      flash[:notice] = "Saldo insuficiente."
+      # redirect_to accounts_path
+    end
+
+    if @transfer.save
+      if @origin.save
+        if @destiny.save
+          flash[:notice] = "Saque realizado."
+          redirect_to accounts_path
+        end
       end
     end
   end
@@ -44,6 +76,10 @@ class TranfersController < ApplicationController
 
   def withdraw_params
     params.require(:transfer).permit(:user_id, :origin_account, :value)
+  end
+
+  def transfer_params
+    params.require(:transfer).permit(:user_id, :origin_account, :destiny_account, :value)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
 
-  get 'deposit' => 'tranfers#deposit'
-  post 'deposit' => 'tranfers#depositar'
+  get 'deposit' => 'transfers#deposit'
+  post 'deposit' => 'transfers#depositar'
 
-  get 'withdraw' => 'tranfers#withdraw'
-  post 'withdraw' => 'tranfers#sacar'
+  get 'withdraw' => 'transfers#withdraw'
+  post 'withdraw' => 'transfers#sacar'
+
+  get 'transfer' => 'transfers#transfer'
+  post 'transfer' => 'transfers#transferir'
 end
