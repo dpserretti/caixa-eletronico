af409bb786526fa52c1e320e1cfd1d214410be57
dpserretti <daniel.dink91@gmail.com>
2016-03-09 17:04:06 -0300

    adiciona testes

---
+++ spec/features/widget_management_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+feature "Testando views tranferências" do
+
+  scenario "Operação Depósito" do
+    visit "/deposit"
+    within("#new_transfer") do
+      fill_in "transfer[destiny]", :with => "4321"
+      fill_in "transfer[value]", :with => "25"
+    end
+    click_button "Depositar"
+    page.should have_content "Depósito realizado."
+  end
+
+  scenario "Operação Saque" do
+    visit "/withdraw"
+    within("#new_transfer") do
+      fill_in "transfer[origin_account]", :with => "1"
+      fill_in "transfer[value]", :with => "50"
+      fill_in "password", :with => "87654321"
+    end
+    click_button "Sacar"
+    page.should have_content "Saque realizado."
+  end
+
+  scenario "Operação Saldo" do
+    visit "/balance"
+    within("#form_balance") do
+      fill_in "account_id[0]", :with => "1"
+    end
+    click_button "Solicitar"
+    page.should have_content "Saldo"
+  end
+
+  scenario "Operação Tranferência" do
+    visit "/transfer"
+    within("#new_transfer") do
+      fill_in "transfer[origin_account]", :with => "1"
+      fill_in "transfer[destiny_account]", :with => "2"
+      fill_in "transfer[value]", :with => "20"
+      fill_in "password", :with => "87654321"
+    end
+    click_button "Transferir"
+    page.should have_content "Tranferência realizada."
+  end
+
+  scenario "Operação Extrato" do
+    visit "/extract"
+    within("#form_extract") do
+      fill_in "account_id[0]", :with => "1"
+      fill_in "data_ini", :with => "01/03/2016"
+      fill_in "data_fin", :with => "10/03/2016"
+    end
+    click_button "Solicitar"
+    page.should have_content "Extrato"
+  end
+
+end
+
+feature "Testando views contas" do
+
+  scenario "Operação Nova Conta" do
+    visit "/accounts/new"
+    within("#new_account") do
+      fill_in "account[number]", :with => "4321"
+      fill_in "account[balance]", :with => "25"
+      fill_in "account[status]", :with => "Ativa"
+    end
+    click_button "Criar"
+    page.should have_content "Conta criada."
+  end
+
+end
