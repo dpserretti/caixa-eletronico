385e5f65e86777ac220bbb251ddec00ab0b94026
dpserretti <daniel.dink91@gmail.com>
2016-03-09 15:10:24 -0300

    altera estrutura

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,6 @@ class Account < ActiveRecord::Base
   belongs_to :users
   validates :user_id, presence: true
   validates :number, presence: true, uniqueness: true
-  validates :balance, presence: true, :numericality => { :greater_than => 0 }
+  validates :balance, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
   validates :status, presence: true
 end
